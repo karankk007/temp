88e1257c97e0fe470e129f6a5e5fd76ccb00adb2
D2_63034_karan <karankadam540@gmail.com>
2022-06-20 16:56:05 +0530

    add

--- page1.js
+++ page1.js
@@ -1,9 +1,14 @@
 console.log('hello world')
 console.log('hello world')
 c my name is karan 
-<<<<<<< HEAD
 from vadgoan 
-=======
 asldjfasdjflaf
 adsfaldsjfajdsf
->>>>>>> temp2
+lasjdfajdflajkdf
+
+
+
+
+
+
+from vadgoan
